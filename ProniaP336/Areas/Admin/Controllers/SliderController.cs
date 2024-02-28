75fe11331c7c4793c1e67b52bcea527dd41245c2
Elton Aliyev <aliyevelton88@gmail.com>
2024-02-28 17:55:10 +0400

    ideal

--- ProniaP336/Areas/Admin/Controllers/SliderController.cs
+++ ProniaP336/Areas/Admin/Controllers/SliderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProniaP336.Contexts;
+using ProniaP336.Models;
 
 namespace ProniaP336.Areas.Admin.Controllers;
 
@@ -20,7 +21,15 @@ namespace ProniaP336.Areas.Admin.Controllers;
         }
 
         public IActionResult Create() { 
-        return View();
+            return View();
         }
+    [HttpPost]
+    public async Task<IActionResult> Create(Slider slider)
+    {
+        await _context.Sliders.AddAsync(slider);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
     }
+}
 
